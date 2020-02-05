7f0d4fc56fbddb2ae0454da28c3ecee48d1432e5
jpco <mail@jpco.io>
2020-02-04 19:12:32 -0800

    Add a landing page, switch main page to /~

--- src/login/index.js
+++ src/login/index.js
@@ -5,7 +5,7 @@ firebase.initializeApp({
 });
 
 var uiConfig = {
-    signInSuccessUrl: '..',
+    signInSuccessUrl: '../~',
     signInOptions: [
         // Leave the lines as is for the providers you want to offer your users.
         // firebase.auth.GoogleAuthProvider.PROVIDER_ID,
